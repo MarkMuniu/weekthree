9d59fd8d27447102b56dca5dd1bec88ad1d3e9bd
MarkMuniu <nyttocreator@gmail.com>
2020-02-24 09:27:32 +0300

    introduce click and toggle on the portfolio images through jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $(".col-md-3").hover(function() {
     $(".fade").fadeIn();
   });
-  $("")
+  $(".clickable").click(function(){
+    $(".col-md-4").toggle();
+  });
 });
 
