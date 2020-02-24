b76a2b8e0a2e93cb1537f9495e32b171732ccbf2
MarkMuniu <nyttocreator@gmail.com>
2020-02-24 22:42:53 +0300

    add a submit button on the 'contact us' section

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
     $(".fade").fadeIn();
   });
   $(".clickable1").click(function(){
-    $(".designtoggle").slideDown();
+    $(".designtoggle").toggle();
   });
   $(".clickable2").click(function(){
-    $(".devtoggle").slideDown();
+    $(".devtoggle").toggle();
+  }); 
+   $(".clickable3").click(function(){ 
+    $(".prodtoggle").toggle();
   });
-   $(".clickable3").click(function(){  
-    $(".prodtoggle").slideDown();
-  });
-  
 });
-
+var userName = document.getElementById("name").value;
+ document.getElementById("submit").innerHTML = "Thank you" + userName + "your message is received."
 
