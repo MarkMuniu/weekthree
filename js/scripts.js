d50ef15fc015c0145bd0b550b719a391e6efa40c
MarkMuniu <nyttocreator@gmail.com>
2020-02-24 22:03:06 +0300

    connect the paragraphs in each icon under their icon

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
   $(".col-md-3").hover(function() {
     $(".fade").fadeIn();
   });
-  $(".clickable").click(function(){
-    $(".designtoggle").toggle();
-    $(".devtoggle").toggle();
-    $(".prodtoggle").toggle();
+  $(".clickable1").click(function(){
+    $(".designtoggle").slideDown();
   });
+  $(".clickable2").click(function(){
+    $(".devtoggle").slideDown();
+  });
+   $(".clickable3").click(function(){  
+    $(".prodtoggle").slideDown();
+  });
+  
 });
+
 
